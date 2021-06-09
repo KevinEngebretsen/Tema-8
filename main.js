29d362c6ef5d24c572843c430f6353d0219b1395
KevinEngebretsen <74900283+KevinEngebretsen@users.noreply.github.com>
2021-06-09 13:07:48 +0200

    1

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function createWindow() {
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 }
-
+icon: path.join(__dirname, "icon/logo.icns");
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
